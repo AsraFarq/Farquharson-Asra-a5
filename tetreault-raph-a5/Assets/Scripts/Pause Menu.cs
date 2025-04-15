5a8a2dcda66cbd96580f31ae30d270d47ee87507
asrafarquharson <cameron.farqu@gmail.com>
2025-04-15 14:03:42 -0400

    ADDED PAUSE MENU

---
+++ tetreault-raph-a5/Assets/Scripts/Pause Menu.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public bool isPaused;
+
+    void update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+    ActivateMenu();
+    Time.timeScale = 0f;
+}
+
+else
+{
+    DeactivateMenu();
+    Time.timeScale = 1f;
+}
+    }
+    void ActivateMenu()
+    {
+        pauseMenuUI.SetActive(true);
+    }
+
+    void DeactivateMenu()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+    }
